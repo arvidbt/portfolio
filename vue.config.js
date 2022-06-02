c194249b0ce84377cfd0bc1b8ef048e83bdf7b5a
Arvid Bergman Thörn <dv20arn@cs.umu.se>
2022-06-02 16:04:19 +0200

    testing script

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/arvid_portfolio/'
+  : '/'
+}
