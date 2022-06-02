afa3abf608085eb7fef34b78c9037540606d2f35
Arvid Bergman Thörn <dv20arn@cs.umu.se>
2022-06-02 16:16:47 +0200

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
-# build
-npm run build
-# navigate into the build output directory
-cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy' --no-verify
-git push -f git@github.com:arvidbt/portfolio.git main:gh-pages
-cd -
+
+git add .
+git commit -m 'auto deploy' --no-verify
+git push -f git@github.com:arvidbt/portfolio.git
--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,3 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/arvid_portfolio/'
-  : '/'
-}
