bf9d282803566b8b92e132a47556a2edbca32a87
arvidbt <dv20arn@cs.umu.se>
2022-06-12 01:21:07 +0200

    skills more doneish

--- src/components/Skills/skillPage.js
+++ src/components/Skills/skillPage.js
@@ -1,3 +1,65 @@
+import { toHandlers } from "vue";
+
 export default {
   name: 'skillPage',
+  data() {
+    return {
+      backend: true,
+      frontend: true,
+      design: true,
+      system: true,
+      both: true,
+      showAll: false,
+    }
+  },
+  methods: {
+    backendSelected() {
+      this.backend = !this.backend;
+      if(this.backend) {
+        this.frontend = false;
+        this.design = false;
+        this.system = false;
+        this.showAll = false;
+      } else {
+        this.showAll = true;
+      }
+    },
+    frontendSelected() {
+      this.frontend = !this.frontend;
+      if(this.frontend) {
+        this.backend = false;
+        this.design = false;
+        this.system = false;
+        this.showAll = false;
+      } else {
+        this.showAll = true;
+      }
+    },
+    designSelected() {
+      this.design = !this.design;
+      if(this.design) {
+        this.frontend = false;
+        this.backend = false;
+        this.system = false;
+        this.showAll = false;
+      } else {
+        this.showAll = true;
+      }
+    },
+    systemSelected() {
+      this.system = !this.system;
+      if(this.system) {
+        this.frontend = false;
+        this.design = false;
+        this.backend = false;
+        this.showAll = false;
+      } else {
+        this.showAll = true;
+      }
+    },
+    isHighlighted(type) {
+      if(this.showAll) return true;
+      return type;
+    },
+  }
 }
--- src/components/Timeline/timeLine.js
+++ src/components/Timeline/timeLine.js
@@ -1,8 +1,24 @@
 export default {
   name: 'timeLine',
+  data() {
+    return {
+      is1997Open: false,
+      is2019Open: false,
+      is2020Open: false,
+      is2021Open: false,
+      is2022Open: false,
+    }
+  },
   methods: {
     linkToCourse(link) {
       window.open(link, 'mywindow')
+    },
+    foldOut(year) {
+      if(year == '1997')      { this.is1997Open = !this.is1997Open }
+      else if(year == '2019') { this.is2019Open = !this.is2019Open }
+      else if(year == '2020') { this.is2020Open = !this.is2020Open }
+      else if(year == '2021') { this.is2021Open = !this.is2021Open }
+      else if(year == '2022') { this.is2022Open = !this.is2022Open }
     }
   }
 }
